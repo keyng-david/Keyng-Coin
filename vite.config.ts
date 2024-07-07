7e254c2fa18fd49233951090d9ab41690b8c2e85
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-07-07 13:43:55 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,8 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
 import checker from "vite-plugin-checker";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -22,5 +20,5 @@ export default defineConfig({
   build: {
     outDir: "./docs",
   },
-  base: "./",
+  base: "/<REPO_NAME>/", // Replace <REPO_NAME> with your GitHub repository name
 });
